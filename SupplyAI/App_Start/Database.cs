a0b35db8ae0f90e84e2fd232fd1e890c9370c88c
Peter <peter1moras@gmail.com>
2019-03-03 13:33:48 -0600

    Connect to online MongoDB

--- SupplyAI/App_Start/Database.cs
+++ SupplyAI/App_Start/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,13 @@ using SupplyAI.Models;
 
 namespace SupplyAI
 {
+    //Class used for accessing MongoDB database w/ methods ofr easy & convenienct loading/storing
     public class Database
     {
         //public const string AppName = "SupplyAI";
-        private static readonly string connectionString = "mongodb://localhost:27017";
+        private static readonly string CONFIGPATH = "App_Data/";
+        private static readonly string CONNECTIONSTRING = "mongodb + srv://{0}:{1}@c54-lnh7c.mongodb.net/test?retryWrites=true";
+        private static readonly string localconnect = "mongodb://localhost:27017";
         public static readonly string DefaultDatabase = "SupplyAI";
         public MongoClient client { get; private set; }
         public MongoDatabaseBase DefaultDB { get { return (MongoDatabaseBase)client.GetDatabase(DefaultDatabase); } }
@@ -20,7 +24,9 @@ namespace SupplyAI
 
 
         public Database() {
-            client = new MongoClient(connectionString);
+
+
+            client = new MongoClient(localconnect);
         }
 
         public MongoDatabaseBase GetDatabase(string name) {
@@ -34,9 +40,34 @@ namespace SupplyAI
             }
             return dbs.ToArray();
         }
+        /// <summary>
+        /// Reads the config file to load in user name and password
+        /// </summary>
+        /// <returns>returns string[2] ={ username, password }</returns>
+        private static string[] loadUserConfig() {
+            string[] config = File.ReadAllLines(CONFIGPATH + "config");
+            string username = "";
+            string password = "";
+            string[] split;
+            foreach(var s in config) {
+                split = s.Split(' '); //get config line
+                if(split.Length ==2) { //if config line has config specification
+                    if (split[0] == "username")
+                        username = split[1];
+                    else if (split[0] == "password")
+                        password = split[1];
+                }
 
+            }
+            return new string[] { username, password };
+        }
+        private static string getConnectionString() {
+            string[] config = loadUserConfig();
+            return String.Format(CONNECTIONSTRING, config[0], config[1]);
+        }
 
     }
+    
     public static class MongoDBExtensions
     {
         public static bool isDatabaseAvailable(this IMongoDatabase database) {
@@ -44,4 +75,5 @@ namespace SupplyAI
         }
 
     }
+  
 }
